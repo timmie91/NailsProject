4b3ed437a2b474efd82677095d22ff315204c279
TIM NGUYEN <Tim@tims-mbp.mobile.asu.edu>
2016-03-29 16:48:43 -0700

    First commit

--- Transaction.swift
+++ Transaction.swift
@@ -2,7 +2,7 @@
 //  Transaction.swift
 //  Nails Keeper
 //
-//  Created by TIM NGUYEN on 3/16/16.
+//  Created by TIM NGUYEN on 3/29/16.
 //  Copyright © 2016 TIM NGUYEN. All rights reserved.
 //
 
@@ -13,15 +13,5 @@ import CoreData
 class Transaction: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
-    @NSManaged var entryTime: NSDate?
-    @NSManaged var workDate: NSDate?
-    @NSManaged var amount: NSDecimalNumber?
-    @NSManaged var tipCard: NSDecimalNumber?
-    @NSManaged var tipCash: NSDecimalNumber?
-    @NSManaged var stubNumber: NSNumber?
-    @NSManaged var isAllDay: NSNumber?
-    @NSManaged var percentage: NSDecimalNumber?
-    @NSManaged var transID:NSNumber?
-    
-}
 
+}
